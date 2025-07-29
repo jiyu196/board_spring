eb207fcfa5841b3721a4237ff362b25c54f73a04
jiyu196 <jiyu16413@gmail.com>
2025-07-29 18:25:30 +0900

    promise 객체 개념 및 실습(promise.js 파일로 이동)/ 조회화면에서 댓글 처리

--- src/main/java/com/kiylab/board/controller/ReplyController.java
+++ src/main/java/com/kiylab/board/controller/ReplyController.java
@@ -1,15 +1,13 @@
 package com.kiylab.board.controller;
 
+import com.kiylab.board.domain.dto.ReplyDTO;
 import com.kiylab.board.service.ReplyService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -22,6 +20,18 @@ public class ReplyController {
   @GetMapping(value = "board/{bno}")
   public ResponseEntity<?> getlist(@PathVariable("bno") Long bno) {
     return ResponseEntity.ok(replyService.getlist(bno));
+  }
+
+  @PostMapping("")
+  public ResponseEntity<?> createReply(@RequestBody ReplyDTO dto) {
+    log.info(dto);
+    return ResponseEntity.ok(replyService.register(dto));
+  }
+
+  @DeleteMapping("board/{rno}")
+  public ResponseEntity<?> deleteReply(@PathVariable("rno") Long rno) {
+    replyService.remove(rno);
+    return ResponseEntity.ok("sdsd");
   }
 
 }
